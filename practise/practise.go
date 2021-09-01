56031999b9f83bd7d450ad5dbb416ae430aac66e
zhengqing.zhu <zhengqing.zhu@tcl.com>
2021-09-01 11:43:35 +0800

    1

--- practise/practise.go
+++ practise/practise.go
@@ -2,9 +2,23 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("hello world")
+type treeNode struct {
+	value       int
+	left, right *treeNode
+}
 
-	num := 1 << 4
-	fmt.Println(num)
+func createTreeNode(v int) *treeNode {
+	return &treeNode{v, nil, nil}
+}
+
+func main() {
+	root := treeNode{2, nil, nil}
+	root.left = &treeNode{3, &treeNode{2, nil, nil}, nil}
+	root.right = createTreeNode(10)
+
+	nodes := []treeNode{
+		{2, nil, &root},
+	}
+
+	fmt.Println(nodes)
 }
